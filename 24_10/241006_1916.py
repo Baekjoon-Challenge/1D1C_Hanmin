db5957fac15f75f3497d580e8e078c3bf7928424
sohanmin <world2013@naver.com>
2024-10-06 19:21:56 +0900

    241006

---
+++ 24_10/241006_1916.py
@@ -0,0 +1,45 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+inf = 10000000*n
+
+distance = [inf for i in range(n+1)]
+visited = [False for i in range(n+1)]
+graph = [[] for j in range(n+1)]
+for i in range(m):
+    start, end, cost = map(int,sys.stdin.readline().split())
+    graph[start].append([end, cost]) 
+
+s, e = map(int,sys.stdin.readline().split())
+
+
+
+def dijkstra(start,end):
+    distance[start] = 0
+    visited[start] = True
+    
+    for i in graph[start]:
+        distance[i[0]] = min(distance[i[0]],i[1])
+    if visited[e] == True:
+        return
+    
+    while visited[end] != True:
+        min_dis = inf
+        index = 0
+        for i in range(1,n+1):
+            if visited[i] == True: continue
+            if distance[i] < min_dis:
+                min_dis = distance[i]
+                index = i
+        visited[index] = True
+        for j in graph[index]:
+            distance[j[0]] = min(distance[index] + j[1],distance[j[0]])
+        print(distance)
+
+
+
+dijkstra(s,e)
+
+print(distance[e])
