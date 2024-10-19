b55a05985d2b118a1328065be7805de1da3178fd
sohanmin <world2013@naver.com>
2024-10-19 23:04:55 +0900

    241019

---
+++ 24_10/241019_11404.py
@@ -0,0 +1,35 @@
+import sys
+
+INF = 1e9
+
+n = int(sys.stdin.readline())
+m = int(sys.stdin.readline())
+
+graph = [[INF for i in range(n+1)] for j in range(n+1)]
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i == j:
+            graph[i][j] = 0
+
+
+for i in range(m):
+    a, b, c = map(int,sys.stdin.readline().split())
+    if graph[a][b] > c:
+        graph[a][b] = c
+
+
+for k in range(1, n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b],graph[a][k] + graph[k][b])
+
+
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print("0",end=" ")
+        else:
+            print(graph[i][j], end=" ")
+    print()
+
